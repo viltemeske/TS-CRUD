f19dfdd90efb0404feeacc6c20e0be653e296cf3
viltemeske <meshkenai@gmail.com>
2023-01-24 22:11:01 +0200

    commit: 8(thead, tbody)

--- src/components/table.ts
+++ src/components/table.ts
@@ -9,8 +9,16 @@ type TableProps<Type> = {
       private props: TableProps;
 
       private tbody: HTMLTableSectionElement;
-      
+
       private thead: HTMLTableSectionElement;
       
       public htmlElement: HTMLTableElement;
+
+      public constructor(props: TableProps<Type>) {
+        this.props = props;    
+        this.htmlElement = document.createElement('table');
+        this.thead = document.createElement('thead');
+        this.tbody = document.createElement('tbody');
   }
+
+}
