68efb20516458d97dffa74325b8c8882a30a0057
viltemeske <meshkenai@gmail.com>
2023-01-24 19:00:04 +0200

    commit: 3 (cars-collection)

--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -1,9 +1,20 @@
-type CarsCollectionProps = {
+import Car from '../types/car';
+import Model from '../types/model';
+import Brand from '../types/brand';
+import CarJoined from '../types/car-joined';
 
+type CarsCollectionProps = {
+    cars: Car[],
+    brands: Brand[],
+    models: Model[],
 };
 
 class CarsCollection {
+    private props: CarsCollectionProps;
 
+    constructor(props: CarsCollectionProps) {
+        this.props = props;
+
+    }
 }
-
 export default CarsCollection;
