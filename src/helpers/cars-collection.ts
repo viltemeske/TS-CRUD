e00fe9b4f8a055d3a6af290dfe6729a495503f2c
viltemeske <meshkenai@gmail.com>
2023-01-25 22:34:57 +0200

    commit: task 2 (getByBrandID)

--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -31,5 +31,19 @@ class CarsCollection {
       public get all(): CarJoined[] {
         return this.props.cars.map(this.joinCar);
       }
+
+      public getByBrandId = (brandId: string): CarJoined[] => {
+        const { cars, models } = this.props;
+
+        const brandModelsIds = models
+          .filter((model) => model.brandId === brandId)
+          .map((model) => model.id);
+
+        const brandCars = cars
+          .filter((car) => brandModelsIds.includes(car.modelId))
+          .map(this.joinCar);
+
+        return brandCars;
+      };
 }
 export default CarsCollection;
